aa9eb298722b82936b021c72ea6df9c18abb3781
jonathan <tryweb@ichiayi.com>
2023-04-07 16:35:15 +0800

    Fixed for Cross-Site Request Forgery (CSRF) issue

--- app/main.py
+++ app/main.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+# 啟用 CSRF 保護機制
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def home():
-    # 使用template資料夾內的home.html當作首頁
     return render_template("home.html")
-    
+
 @app.route("/about")
 def about():
-    # 使用template資料夾內的about.html當作首頁
     return render_template("about.html")
-    
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
